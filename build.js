795bb110b6c4f74c1c9d4f09f26ae67ffd67c3ac
David Acevedo <david.acevedo07@gmail.com>
2017-02-16 00:15:49 -0800

    Get rid of logs

--- build.js
+++ build.js
@@ -1,7 +1,4 @@
 import webpack from 'webpack';
 import webpackConfig from './webpack/webpack.config';
 
-const compiler = webpack(webpackConfig, (err, stats) => {
-  console.log(err);
-  console.log(stats);
-});
+const compiler = webpack(webpackConfig, (err, stats) => {});
