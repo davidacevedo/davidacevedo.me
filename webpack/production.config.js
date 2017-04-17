35b6b311ed794d1810a3ee41d69925f89544cb40
David Acevedo <david.acevedo07@gmail.com>
2017-04-17 00:42:00 -0700

    Change minification settings

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,19 +1,22 @@
 import webpack from 'webpack';
 
 module.exports = {
-    devtool: 'cheap-module-source-map',
-    output: {
-      filename: 'bundle.js',
-      path: `${__dirname}/../dist`,
-    },
+  devtool: 'cheap-module-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/../dist`,
+  },
 
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin(),
-      new webpack.LoaderOptionsPlugin({
-        minimize: true
-      }),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      })
-    ]
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 }
